d0722812812e519d59b9e4385751d58b71c7fe92
kokum007 <dakokum@gmail.com>
2021-03-27 23:47:03 +0530

    crate signup form

--- app/src/main/java/com/example/emeter/Bill.java
+++ app/src/main/java/com/example/emeter/Bill.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class BillPage extends AppCompatActivity {
+public class Bill extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_billpage);
+        setContentView(R.layout.activity_bill);
     }
 }
